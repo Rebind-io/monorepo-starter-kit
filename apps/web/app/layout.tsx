d69bb1585c0f9fd44347067a98998d2eb564384f
mhShohan <mehdihasanshohan25@gmail.com>
2026-01-17 14:49:57 +0600

    fix - color themes

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,24 @@
 import { Providers } from '@/components/providers';
 
 import '@workspace/ui/globals.css';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { IBM_Plex_Mono, Inter, Libre_Baskerville } from 'next/font/google';
 
-const fontSans = Geist({
+const fontSans = Inter({
   subsets: ['latin'],
   variable: '--font-sans',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
-const fontMono = Geist_Mono({
+const fontSerif = Libre_Baskerville({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ['400', '700'],
+});
+
+const fontMono = IBM_Plex_Mono({
   subsets: ['latin'],
   variable: '--font-mono',
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
 });
 
 export default function RootLayout({
@@ -21,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased`}
+        className={`${fontSans.variable} ${fontMono.variable} ${fontSerif.variable} font-sans antialiased`}
       >
         <Providers>{children}</Providers>
       </body>
